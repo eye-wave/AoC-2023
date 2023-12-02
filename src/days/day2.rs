2d1c0b634eac4fe3dc29a046107aff9612ba3c98
_eyewave <89079979+eye-wave@users.noreply.github.com>
2023-12-02 12:12:22 +0100

    chore: cargo fmt

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -11,22 +11,20 @@ pub fn part1(input: &Vec<String>) -> u16 {
             for group in game.groups.iter() {
                 let mut red = 0;
                 let mut green = 0;
-                let mut blue = 0;        
+                let mut blue = 0;
 
-                group.iter().for_each(|col| {
-                    match col.color {
-                        Colors::Red => red += col.number,
-                        Colors::Green => green += col.number,
-                        Colors::Blue => blue +=col.number,
-                    }
+                group.iter().for_each(|col| match col.color {
+                    Colors::Red => red += col.number,
+                    Colors::Green => green += col.number,
+                    Colors::Blue => blue += col.number,
                 });
 
                 if red > red_count || green > green_count || blue > blue_count {
-                    return 0
+                    return 0;
                 }
             }
 
-            return game.id as u16
+            return game.id as u16;
         })
         .sum()
 }
@@ -37,18 +35,37 @@ pub fn part2(input: &Vec<String>) -> u32 {
         .map(|line| {
             let game = get_sets_from_line(line);
 
-            let colors = game.groups.into_iter().flatten().collect::<Vec<ColorCount>>();
-            
-            let red = colors.iter().filter(|col| col.color == Colors::Red).max_by_key(|col| col.number).unwrap().number as usize;
-            let green = colors.iter().filter(|col| col.color == Colors::Green).max_by_key(|col| col.number).unwrap().number as usize;
-            let blue = colors.iter().filter(|col| col.color == Colors::Blue).max_by_key(|col| col.number).unwrap().number as usize;
+            let colors = game
+                .groups
+                .into_iter()
+                .flatten()
+                .collect::<Vec<ColorCount>>();
+
+            let red = colors
+                .iter()
+                .filter(|col| col.color == Colors::Red)
+                .max_by_key(|col| col.number)
+                .unwrap()
+                .number as usize;
+            let green = colors
+                .iter()
+                .filter(|col| col.color == Colors::Green)
+                .max_by_key(|col| col.number)
+                .unwrap()
+                .number as usize;
+            let blue = colors
+                .iter()
+                .filter(|col| col.color == Colors::Blue)
+                .max_by_key(|col| col.number)
+                .unwrap()
+                .number as usize;
 
             (red * green * blue) as u32
         })
         .sum()
 }
 
-#[derive(Debug,PartialEq)]
+#[derive(Debug, PartialEq)]
 enum Colors {
     Red,
     Blue,
@@ -68,17 +85,21 @@ struct Game {
 }
 
 fn get_sets_from_line(line: &str) -> Game {
-    let split:Vec<&str> = line.split(":").collect();
+    let split: Vec<&str> = line.split(":").collect();
     let label_str = split.first().unwrap();
-    let game_id = label_str.replace("Game ","").parse::<u8>().unwrap();
-    
+    let game_id = label_str.replace("Game ", "").parse::<u8>().unwrap();
+
     let groups_str = split.last().unwrap().trim();
     let groups_vec = groups_str.split(";").collect::<Vec<&str>>();
-    let groups:Vec<Vec<ColorCount>> = groups_vec.iter()
+    let groups: Vec<Vec<ColorCount>> = groups_vec
+        .iter()
         .map(|group| {
-            group.split(",").collect::<Vec<&str>>().iter()
+            group
+                .split(",")
+                .collect::<Vec<&str>>()
+                .iter()
                 .map(|color| {
-                    let split:Vec<&str> = color.trim().split_whitespace().collect();
+                    let split: Vec<&str> = color.trim().split_whitespace().collect();
                     let number = split.first().unwrap().parse::<u8>().unwrap();
                     let col = split.last().unwrap().to_owned();
 
